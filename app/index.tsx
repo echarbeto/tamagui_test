b3822dc5aa8681661da4831d72c10ddfe9b74e0c
echarbeto <eleazargg@gmail.com>
2023-10-02 23:39:53 -0300

    Example

--- app/index.tsx
+++ app/index.tsx
@@ -1,62 +1,51 @@
 import { useRouter } from "expo-router";
 import {
-  Button,
-  H1, Input, Label,
-  Paragraph,
-  SizableText, Theme,
   YStack
 } from "tamagui";
+import { View, Text } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 import { MyStack } from "../components/MyStack";
 
 export default function Home() {
   const router = useRouter();
 
+  type ItemProps = {
+    item: {
+      text: string;
+    };
+  };
+
+  const data = Array.from({length: 50}, (_, i) => ({id: i, text: `Item ${i}`}));
+
+  const Item = ({ item }: ItemProps) => {
+  return (
+    <Swipeable
+      renderRightActions={() => (
+        <View style={{ width: 100, backgroundColor: 'red', justifyContent: 'center' }}>
+          <Text style={{ color: 'white', textAlign: 'center' }}>Delete</Text>
+        </View>
+      )}
+    >
+      <View style={{ height: 50, backgroundColor: 'white', justifyContent: 'center' }}>
+        <Text>{item.text}</Text>
+      </View>
+    </Swipeable>
+  );
+};
+
   return (
     <MyStack>
       <YStack
         space="$5"
       >
-        <YStack space="$5">
-        <Theme>
-          <H1 textAlign="center">Theme name=null</H1>
-          <Label>Label theme=null</Label>
-          <Label theme="alt1">Label theme=alt1</Label>
-          <Label theme="alt2">Label theme=alt2</Label>
-          <Label theme="active">Label theme=active</Label>
-          <Input />
-          <Input theme="alt1" />
-          <Input theme="alt2" />
-          <Input theme="active" />
-          <Button>Button theme=null</Button>
-          <Button theme="alt1">Button theme=alt1</Button>
-          <Button theme="alt2">Button theme=alt2</Button>
-          <Button theme="active">Button theme=active</Button>
-          <SizableText>SizableText theme=null</SizableText>
-          <SizableText theme="alt1">SizableText theme=alt1</SizableText>
-          <SizableText theme="alt2">SizableText theme=alt2</SizableText>
-        </Theme>
-      </YStack>
-      <YStack space="$5">
-          <Theme name="red">
-            <H1 textAlign="center">Theme name=red</H1>
-            <Label>Label theme=null</Label>
-            <Label theme="alt1">Label theme=alt1</Label>
-            <Label theme="alt2">Label theme=alt2</Label>
-            <Label theme="active">Label theme=active</Label>
-            <Input />
-            <Input theme="alt1" />
-            <Input theme="alt2" />
-            <Input theme="active" />
-            <Button>Button theme=null</Button>
-            <Button theme="alt1">Button theme=alt1</Button>
-            <Button theme="alt2">Button theme=alt2</Button>
-            <Button theme="active">Button theme=active</Button>
-            <SizableText>SizableText theme=null</SizableText>
-            <SizableText theme="alt1">SizableText theme=alt1</SizableText>
-            <SizableText theme="alt2">SizableText theme=alt2</SizableText>
-          </Theme>
-      </YStack>
+        <FlatList
+      data={data}
+      keyExtractor={item => item.id.toString()}
+      renderItem={({ item }) => <Item item={item} />}
+      style={{ maxHeight: 400 }}
+    />
       </YStack>
     </MyStack>
   );
